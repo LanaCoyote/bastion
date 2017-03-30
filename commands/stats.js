4c20d9853eb3f8ae4d0683ce69a6a9d5ad7b7ad9
David Reeve <nerdboy6@gmail.com>
2017-03-30 01:24:57 -0400

    can now get stats from a user mention

--- commands/stats.js
+++ commands/stats.js
@@ -1,24 +1,58 @@
 const overwatch = require('overwatch-js');
+const Promise = require('bluebird');
 
+const battletag = require('../lib/battletagStorage');
 const drawStatGrid = require('../lib/statgrid');
 const log = require('../lib/log');
 
 const battleTagPattern = /^\w+#|-\d{3,6}$/;
 
+function getBattleTag(message, params) {
+    const user = message.mentions.users.first();
+    if (params[0] && !user) {
+        if (!battleTagPattern.test(params[0])) return message.reply("the Battletag you provided is invalid!");
+        return Promise.resolve(params[0]);
+    } else {
+        const userId = user.id || message.author.id;
+        if (userId === "296764768197279744") return Promise.resolve('self');
+        return battletag.fetchBattleTag(userId);
+    }
+}
+
 function getProfileByTag(tag, region) {
     if (!tag) throw new Error("Attempted to get Overwatch profile where no tag was provided");
     if (tag.includes('#')) tag = tag.replace('#', '-');
     region = region || "us";
+    if (tag === 'self') return Promise.resolve(fakeAssProfile());
 
     return overwatch.getAll("pc", region, tag)
 }
 
-function collectStats(message, params) {
-    if (!params[0]) return message.reply("you must provide a Battletag!");
-    if (!battleTagPattern.test(params[0])) return message.reply("the Battletag you provided is invalid!");
+function fakeAssProfile() {
+    return {
+        profile: {nick: "SaltPoweredRobot", level: 99, rank: 5096, avatar: "https://blzgdapipro-a.akamaihd.net/game/unlocks/0x02500000000008B8.png", url: ""},
+        competitive: {global: {
+            solo_kills_average: 69,
+            eliminations_average: 69,
+            damage_done_average: 420000,
+            deaths_average: 0,
+            final_blows_average: 69,
+            healing_done_average: 0,
+            objective_kills_average: 42,
+            objective_time_average: 20,
+            games_won: 1,
+            games_lost: 0
+        }}
+    };
+}
 
-    try {
-        return getProfileByTag(params[0], params[1])
+function collectStats(message, params) {
+    return getBattleTag(message, params).then(battleTag => {
+        if (battleTag === null) return params[0] ?
+            message.reply("that user does not have a Battletag set") :
+            message.reply("you don't have a Battletag set! Use `!battletag set <your battletag>` to set one");
+        if (typeof battleTag !== "string") return;
+        return getProfileByTag(battleTag, params[1])
             .then(profile => {
                 return drawStatGrid(profile).then(buffer => {
                     const response = `Stats for ${profile.profile.nick} | Level ${profile.profile.level} | `
@@ -36,9 +70,7 @@ function collectStats(message, params) {
                         .then(lana => lana && lana.sendMessage("Error: " + message.content));
                 }
             });
-    } catch (err) {
-        log.error(err);
-    }
+    });
 }
 
 module.exports = {
